a4809170985056364cddd94117c15d0306b4deb8
MarynaIlchenko <debovamail@gmail.com>
2022-01-20 17:46:05 +0200

    Update task 1_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+// console.log(createImageCardsMarkup(galleryItems));
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onImagesContainerClick);
 
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original }) => {
+        return `      
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="Image description" />
+        </a>
+      </div>
+      `;
+    }).join('');
+}
+
+function onImagesContainerClick(e) {
+    const isGalleryItemEl = e.target.classList.contains('gallery__item');
+ if (!isGalleryItemEl) {
+        return;
+ }
+        console.log(e.target);
+}
 console.log(galleryItems);
