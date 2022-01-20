648447f27a026be134b46e5f8e3ccf8bfed9eb23
MarynaIlchenko <debovamail@gmail.com>
2022-01-20 22:08:49 +0200

    Update-task-07_3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,11 +8,11 @@ imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 imagesContainer.addEventListener('click', onImagesContainerClick);
 
 function createImageCardsMarkup(galleryItems) {
-    return galleryItems.map(({ preview, original }) => {
+    return galleryItems.map(({ preview, original, description }) => {
         return `      
     <div class="gallery__item">
         <a class="gallery__link" href="${original}" data-lightbox="pictures">
-            <img class="gallery__image" src="${preview}" data-source="${original}" alt="Image description" />
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
         </a>
       </div>
       `;
@@ -32,12 +32,7 @@ function onImagesContainerClick(e) {
 console.log(galleryItems);
 
 const instance = basicLightbox.create(`
-    <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
-    </div>
-`)
+    <img src="${imageSrc}" width="800" height="600">
+    `)
 
 instance.show()
