22ba9c957e342f4487da84f2145bea1abc6ad224
MarynaIlchenko <debovamail@gmail.com>
2022-01-20 21:41:37 +0200

    Update-task 7_2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const imagesContainer = document.querySelector('.gallery');
 const cardsMarkup = createImageCardsMarkup(galleryItems);
+
 // console.log(createImageCardsMarkup(galleryItems));
 imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 imagesContainer.addEventListener('click', onImagesContainerClick);
@@ -10,8 +11,8 @@ function createImageCardsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original }) => {
         return `      
     <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img class="gallery__image" src="${preview}" data-source="${original}" alt="Image description" />
+        <a class="gallery__link" href="${original}" data-lightbox="pictures">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="Image description" />
         </a>
       </div>
       `;
@@ -23,6 +24,20 @@ function onImagesContainerClick(e) {
  if (!isGalleryItemEl) {
         return;
  }
-        console.log(e.target);
+    const itemEl = e.target;
+    const parentColorCard = itemEl.closest('.gallery-item');
+    parentColorCard.classList.add('is-active');//??
+        // console.log(e.target);
 }
 console.log(galleryItems);
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instance.show()
