62c4be843c49c2cda6a1f9f25a47873a2cbc68c7
MarynaIlchenko <debovamail@gmail.com>
2022-01-20 22:16:55 +0200

    Update_07_4

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,28 +11,30 @@ function createImageCardsMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `      
     <div class="gallery__item">
-        <a class="gallery__link" href="${original}" data-lightbox="pictures">
+        <a class="gallery__link" href="${original}">
             <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
         </a>
       </div>
-      `;
+      `
     }).join('');
 }
 
 function onImagesContainerClick(e) {
-    const isGalleryItemEl = e.target.classList.contains('gallery__item');
- if (!isGalleryItemEl) {
+    const isGalleryItemEl = e.target.classList.contains('gallery__image');
+    const imageSrc = e.target.dataset.source;
+    e.preventDefault();
+    if (!isGalleryItemEl) {
         return;
- }
-    const itemEl = e.target;
-    const parentColorCard = itemEl.closest('.gallery-item');
-    parentColorCard.classList.add('is-active');//??
-        // console.log(e.target);
-}
-console.log(galleryItems);
+    }
+    //     const itemEl = e.target;
+    //     const parentColorCard = itemEl.closest('.gallery-item');
+    //     parentColorCard.classList.add('is-active');//??
+    //         // console.log(e.target);
+    // }
+    // console.log(galleryItems);
 
-const instance = basicLightbox.create(`
+    const instance = basicLightbox.create(`
     <img src="${imageSrc}" width="800" height="600">
     `)
-
-instance.show()
+    instance.show()
+}
