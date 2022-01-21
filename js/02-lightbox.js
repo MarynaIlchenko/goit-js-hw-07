e25f44921a2450e009f6aa362e5a894adb9c0253
MarynaIlchenko <debovamail@gmail.com>
2022-01-21 12:27:21 +0200

    Update task 7(2)_1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onImagesContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `      
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `
+    }).join('');
+}
+
+function onImagesContainerClick(e) {
+    const isGalleryItemEl = e.target.classList.contains('gallery__image');
+    const imageSrc = e.target.dataset.source;
+    e.preventDefault();
+    if (!isGalleryItemEl) {
+        return;
+    }
+
+
+    console.log(galleryItems);
